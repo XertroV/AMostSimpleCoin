033fb7aad49a6fe3914579c18081256465b70bb3
Max Kaye <m@xk.io>
2014-10-09 13:13:35 +1100

    forgot to add lua stuff for state (and orphanage too, now)

--- lua_helpers.py
+++ lua_helpers.py
@@ -14,14 +14,18 @@ local copy_hashmap = function (from, to)
         redis.call("HMSET", to, unpack(redis.call("HGETALL", from)))
     end
 end
-
-
 """
 
 def make_script(r, lua_code, path, **kwargs):
     #print("Making", lua_code.format(path=path, **kwargs))
     return r.register_script(_functions + lua_code.format(path=path, **kwargs))
 
+
+#
+# State
+#
+
+# Backup / restore
 
 _backup_state = """
     copy_hashmap('{path}', KEYS[1])
@@ -37,6 +41,8 @@ _restore_backup_state = """
 
 def get_restore_backup_state_function(r, path, backup_path):
     return make_script(r, _restore_backup_state, path, backup_path=backup_path)
+
+# Balance
 
 _mod_balance = """
     local n
@@ -63,3 +69,57 @@ _get_balance = """
 
 def get_get_balance(r, path):
     return make_script(r, _get_balance, path)
+
+
+#
+# Orphanage
+
+"""
+local orph_contains = function (path, block_hash)
+  return redis.call("SISMEMBER", gen_set_path(path), block_hash)
+end
+
+local orph_get = function(path, block_hash)
+  return redis.call("HGET", gen_orph_map_path(path), block_hash)
+end
+
+local orphs_linking_to = function(block_hash)
+  local set_key = redis.call("HGET", gen_rev_map_path(path), block_hash)
+  return redis.call("SMEMBERS", gen_rev_key_path(path, block_hash))
+end
+
+--[[ Modification ]]
+
+local orph_add = function (path, block, block_hash, linked_block_hash)
+  redis.call("SADD", gen_set_path(path), block_hash)
+  redis.call("HSET", gen_orph_map_path(path), block_hash, block)
+  redis.call("HSET", gen_rev_map_path(path), linked_block_hash, gen_rev_key_path(path, linked_block_hash))
+  redis.call("SADD", gen_rev_key_path(path, linked_block_hash), block_hash)
+end
+
+local orph_remove = function (path, block_hash)
+  redis.call("SREM", gen_set_path(path), block_hash)
+  redis.call("HDEL", gen_orph_map_path(path), block_hash)
+  redis.call("SREM", gen_rev_key_path(path, linked_block_hash), block_hash)
+  if redis.call("EXISTS", gen_rev_key_path(path, linked_block_hash)) == 0 then
+    redis.call("HDEL", gen_rev_map_path(path), linked_block_hash, gen_rev_key_path(path, linked_block_hash))
+  end
+end"""
+#
+
+with open('lua_scripts/orphanage.lua') as f:
+    orphanage_functions = f.read()
+
+def make_orphanage_script(r, lua_code, path):
+    return make_script(r, orphanage_functions + lua_code, path)
+
+
+_orphanage_add = """
+    return orph_add("{path}", KEYS[1], KEYS[2], KEYS[3])
+"""
+
+def get_orphanage_add(r, path):
+    # KEYS[1,2,3] are block, block_hash, linked_block_hash respectively
+    return make_orphanage_script(r, _orphanage_add, path)
+
+_orphanage_add
